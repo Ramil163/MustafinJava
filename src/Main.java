017c7a127823cde07fe57dff823da20046bac5b3
Ramil163 <mustafin.rf@yandex.ru>
2025-04-15 16:48:29 +0400

    feat: done all the task

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,25 @@ public class Main {
 
         System.out.println("Результат сложения: " + (a + b) + "\nРезультат вычитания: " + (a - b) + "\nРезультат деления: " + (a / b ) +  "\nРезультат умножения: " + (a*b));
 
+        System.out.print("Введите строку a: ");
+        String A = scan.nextLine();
+        A = scan.nextLine();
+
+        System.out.print("Введите строку b: ");
+        String B = scan.nextLine();
+        if (A.equals(B)) {
+         System.out.println("Строки идентичны");
+        }
+        else {
+            System.out.println("Строки неидентичны");
+        }
+
+
+        int[] numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,};
+        for (int i = 0; i < numbers.length; i++) {
+            if((i % 2) != 0) {System.out.println(numbers[i]);}
+        };
+
 
 
 
