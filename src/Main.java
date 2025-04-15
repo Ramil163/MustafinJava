695975933cbf01005d6b9d879c23bcdcec946623
Ramil163 <mustafin.rf@yandex.ru>
2025-04-15 17:38:59 +0400

    fix: fix a bag in 3 part

--- src/Main.java
+++ src/Main.java
@@ -42,7 +42,7 @@ public class Main {
 
         int[] numbers = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,};
         for (int i = 0; i < numbers.length; i++) {
-            if((i % 2) != 0) {System.out.println(numbers[i]);}
+            if((numbers[i] % 2) == 0) {System.out.println(numbers[i]);}
         };
 
 
